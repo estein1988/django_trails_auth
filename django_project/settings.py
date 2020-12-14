58f998b72c0a6320e86c27d2116b27996a697250
estein1988 <estein1988@gmail.com>
2020-12-14 08:31:45 -0700

    Add cors headers.

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_app',
-    'rest_framework'
+    'rest_framework', 
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
